089954cda9b372949482b6decba0b58b0575781a
paed01 <pal.edman@outlook.com>
2015-02-16 06:35:33 +0100

    Increased test timeout

--- test/auto.js
+++ test/auto.js
@@ -2,15 +2,14 @@ var AzureStorage = require('azure-storage');
 var Lab = require('lab');
 var lab = exports.lab = Lab.script();
 
-var options = {
-	connection : process.env.AZURE_TABLE_CONN || 'UseDevelopmentStorage=true;DevelopmentStorageProxyUri=http://127.0.0.1;',
-	partition : 'unittestcache'
-};
+var connection = process.env.AZURE_TABLE_CONN || 'UseDevelopmentStorage=true;DevelopmentStorageProxyUri=http://127.0.0.1;';
 
-var client = AzureStorage.createTableService(options.connection);
-
-lab.experiment('AzureStorage', function() {
-    lab.test('warming up', function(done) {
-        client.createTableIfNotExists(options.partition, done);
+lab.experiment('warming up', function() {
+    var client = AzureStorage.createTableService(connection);
+    lab.test('regular tests', function(done) {
+        client.createTableIfNotExists('unittestcache', done);
+    });
+    lab.test('gc tests', function(done) {
+        client.createTableIfNotExists('unittestcachegc', done);
     });
 });
