e61b0a2046762845cb364878badf6e75d0848f02
Pål Edman <pal.edman@gmail.com>
2015-12-21 23:47:33 +0100

    Use es6

--- test/auto.js
+++ test/auto.js
@@ -1,17 +1,17 @@
 'use strict';
 
-var AzureStorage = require('azure-storage');
-var Lab = require('lab');
-var lab = exports.lab = Lab.script();
+const AzureStorage = require('azure-storage');
+const Lab = require('lab');
+const lab = exports.lab = Lab.script();
 
-var connection = process.env.AZURE_TABLE_CONN || 'UseDevelopmentStorage=true;DevelopmentStorageProxyUri=http://127.0.0.1;';
+const connection = process.env.AZURE_TABLE_CONN || 'UseDevelopmentStorage=true;DevelopmentStorageProxyUri=http://127.0.0.1;';
 
-lab.experiment('warming up', function () {
-    var client = AzureStorage.createTableService(connection);
-    lab.test('regular tests', function (done) {
-        client.createTableIfNotExists('unittestcache', done);
-    });
-    lab.test('gc tests', function (done) {
-        client.createTableIfNotExists('unittestcachegc', done);
-    });
+lab.experiment('warming up', () => {
+  const client = AzureStorage.createTableService(connection);
+  lab.test('regular tests', (done) => {
+    client.createTableIfNotExists('unittestcache', done);
+  });
+  lab.test('gc tests', (done) => {
+    client.createTableIfNotExists('unittestcachegc', done);
+  });
 });
